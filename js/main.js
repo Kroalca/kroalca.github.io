b56662746458e518915ac962645d858d03cc8c7d
Alejandro Rodríguez Carrillo <kroalca@gmail.com>
2021-09-08 17:23:45 +0100

    Base de la pagina terminada
    Toda la base terminada y resposive, falta cambiar about, animaciones y enlaces

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.typed', {
+    // strings: ['Web Developer', 'Frontend', 'Backend', 'Fullstack'],
+    stringsElement: '#cadena-texto',
+    smartBackspace: false,
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html'
+});
