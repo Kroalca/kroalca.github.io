ed25d6d5989d521afb5fc7ed734ea14bc28aece1
Alejandro Rodríguez Carrillo <kroalca@gmail.com>
2021-09-15 15:36:28 +0100

    php correo

--- assets/mail.php
+++ assets/mail.php
@@ -1,10 +1,10 @@
 <?php
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
+$subject = $_POST['subject'];
 $mensaje = $_POST['mensaje'];
 
 $to = "kroalca@gmail.com";
-$subject = "Web Alejandro";
 $header = "From: ".$email." Name: ".$nombre."\r\n";
 $header .= "Reply-To: noreply@example.com"."\r\n";
 $header .= "X-Mailer: PHP/".phpversion();
