afc1504de6afa24232099689a673250c30877b27
Alejandro Rodríguez Carrillo <kroalca@gmail.com>
2021-09-16 20:01:42 +0100

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -46,6 +46,7 @@ form.addEventListener("submit", (e) => {
 			  document.getElementById('contact_name').value = "";
 			  document.getElementById('contact_email').value = "";
 			  document.getElementById('contact_message').value = "";
+			  document.getElementById('contact_subject').value = "";
 })
 
 
